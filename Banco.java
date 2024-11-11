dfdf345a28de1c589b31f4e985ef81191c83b71a
Rogerio Abreu <rogerioabreumar@gmail.com>
2024-11-11 11:47:12 -0300

    depositar

--- Banco.java
+++ Banco.java
@@ -1,14 +1,14 @@
 import java.util.ArrayList;
 
-
 public class Banco {
     String nome;
     ArrayList<ContaBancaria> contas;
 
     public Banco(String nome) {
         this.nome = nome;
-        this.contas = new ArrayList<ContaBancaria>();
+        this.contas = new ArrayList<>();
     }
+
     public ContaBancaria buscarConta(int numeroDaConta, String senha) {
         for (ContaBancaria conta : contas) {
             if (conta.getNumeroDaConta() == numeroDaConta && conta.validarSenha(senha)) {
@@ -19,58 +19,54 @@ public class Banco {
     }
 }
 
-abstract class ContaBancaria{
+abstract class ContaBancaria {
     private int numeroDaConta;
     private String senha;
     protected double saldo;
     private String nome;
     private String cpf;
 
-
-    public ContaBancaria( int numeroDaConta , String senha , double saldo, String nome, String cpf ){
-        this.numeroDaConta =  numeroDaConta;
-        this.saldo = saldo ;
+    public ContaBancaria(int numeroDaConta, String senha, double saldo, String nome, String cpf) {
+        this.numeroDaConta = numeroDaConta;
+        this.saldo = saldo;
         this.nome = nome;
         this.cpf = cpf;
         this.senha = senha;
-        // tipo conta para verificar se a conta é pj ou pf
     }
-    public int getNumeroDaConta() {return numeroDaConta;}
-    public String getNome() {return nome;}
-    public double getSaldo() {return saldo;}
+
+    public int getNumeroDaConta() { return numeroDaConta; }
+    public String getNome() { return nome; }
+    public double getSaldo() { return saldo; }
+
     public boolean validarSenha(String senha) {
         return this.senha.equals(senha);
+    }
+
+
+    public boolean depositar(double valor) {
+        if (valor > 0) {
+            saldo += valor;
+            return true;
+        }
+        return false;
     }
 
     public abstract boolean sacar(double valor);
 }
 
-class ContaBancariaPF extends ContaBancaria{
-    private String cpf;
-
-    public ContaBancariaPF(int numeroDaConta, String senha, double saldo, String nome, String cpf ) {
+class ContaBancariaPF extends ContaBancaria {
+    public ContaBancariaPF(int numeroDaConta, String senha, double saldo, String nome, String cpf) {
         super(numeroDaConta, senha, saldo, nome, cpf);
     }
-
-    @Override
-    public boolean sacar(double valor) {
-        return false;
-    }
 }
-class ContaBancariaPJ extends ContaBancaria{
-    private String cnpj;
 
+class ContaBancariaPJ extends ContaBancaria {
     public ContaBancariaPJ(int numeroDaConta, String senha, double saldo, String nome, String cnpj) {
         super(numeroDaConta, senha, saldo, nome, cnpj);
     }
-
-    @Override
-    public boolean sacar(double valor) {
-        return false;
-    }
 }
-class ContaCorrentePF extends ContaBancariaPF{
 
+class ContaCorrentePF extends ContaBancariaPF {
     public ContaCorrentePF(int numeroDaConta, String senha, double saldo, String nome, String cpf) {
         super(numeroDaConta, senha, saldo, nome, cpf);
     }
@@ -85,51 +81,76 @@ class ContaCorrentePF extends ContaBancariaPF{
     }
 }
 
-
 class ContaPoupancaPF extends ContaBancariaPF {
-
     public ContaPoupancaPF(int numeroDaConta, String senha, double saldo, String nome, String cpf) {
         super(numeroDaConta, senha, saldo, nome, cpf);
     }
 
     @Override
     public boolean sacar(double valor) {
-        if (valor <= saldo) {
+        double saldoMinimo = 50.0;
+        if (valor <= saldo - saldoMinimo) {
             saldo -= valor;
             return true;
         }
         return false;
     }
 }
-class ContaCorrentePJ extends ContaBancariaPJ{
 
+class ContaCorrentePJ extends ContaBancariaPJ {
+    private static final double TAXA_SAQUE = 5.0;
+    private static final double TAXA_DEPOSITO = 2.0;
 
-    public ContaCorrentePJ(int numeroDaConta, String senha, double saldo, String nome, String cpf) {
-        super(numeroDaConta, senha, saldo, nome, cpf);
+    public ContaCorrentePJ(int numeroDaConta, String senha, double saldo, String nome, String cnpj) {
+        super(numeroDaConta, senha, saldo, nome, cnpj);
     }
 
     @Override
     public boolean sacar(double valor) {
-        if (valor <= saldo) {
-            saldo -= valor;
+        double valorTotal = valor + TAXA_SAQUE;
+        if (valorTotal <= saldo) {
+            saldo -= valorTotal;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean depositar(double valor) {
+        if (valor > TAXA_DEPOSITO) {
+            saldo += (valor - TAXA_DEPOSITO);
             return true;
         }
         return false;
     }
 }
 
-class ContaPoupancaPJ extends ContaBancariaPJ{
-    public ContaPoupancaPJ(int numeroDaConta, String senha, double saldo, String nome, String cpf) {
-        super(numeroDaConta, senha, saldo, nome, cpf);
+class ContaPoupancaPJ extends ContaBancariaPJ {
+    private int saquesRealizados;
+    private static final int LIMITE_SAQUES = 3;
+    private static final double LIMITE_DEPOSITO = 10000.0;
+
+    public ContaPoupancaPJ(int numeroDaConta, String senha, double saldo, String nome, String cnpj) {
+        super(numeroDaConta, senha, saldo, nome, cnpj);
+        this.saquesRealizados = 0;
     }
 
     @Override
     public boolean sacar(double valor) {
-        if (valor <= saldo) {
+        if (saquesRealizados < LIMITE_SAQUES && valor <= saldo) {
             saldo -= valor;
+            saquesRealizados++;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean depositar(double valor) {
+        if (valor > 0 && valor <= LIMITE_DEPOSITO) {
+            saldo += valor;
             return true;
         }
         return false;
     }
 }
-
